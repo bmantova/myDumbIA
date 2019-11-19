7de37947d018ee71f0478cb2dd0d4de9abfb0204
bmantova <baptiste.mantovani@laposte.net>
2019-11-19 12:07:47 +0100

    get the height of the map

--- src/webgl/objects/ground/Ground.js
+++ src/webgl/objects/ground/Ground.js
@@ -16,39 +16,48 @@ export default class Ground extends Object3D {
     this.humidity = new Map(constants.GROUND.MAPS.HUMIDITY)
     this.temperature = new Map(constants.GROUND.MAPS.TEMPERATURE)
 
-    this.geometry = new PlaneBufferGeometry(constants.GROUND.SIZE, constants.GROUND.SIZE, constants.GROUND.SUB, constants.GROUND.SIZE)
+    this.geometry = new PlaneBufferGeometry(constants.GROUND.SIZE, constants.GROUND.SIZE, constants.GROUND.SUB, constants.GROUND.SUB)
 
     this.material = new RawShaderMaterial({
       uniforms: {},
       vertexShader: vertexShader,
       fragmentShader: fragmentShader,
-      side: DoubleSide
+      side: DoubleSide,
+      wireframe: true
     })
 
     const vertices = this.geometry.attributes.position.array
 
-    for (let i = 0; i < vertices.length; i++) {
-      vertices[i * 3 + 2] = this.height.get(i % constants.GROUND.SUB, Math.floor(i / constants.GROUND.SUB)) * 10
+    /* for (let i = 0; i < vertices.length; i++) {
+      vertices[i * 3 + 2] = this.height.get((i * 3) % (constants.GROUND.SUB + 1) + 3, Math.floor((i * 3) / (constants.GROUND.SUB + 1))) * 10
+    } */
+
+    for (let i = 0; i <= constants.GROUND.SUB; i++) {
+      for (let j = 0; j <= constants.GROUND.SUB; j++) {
+        vertices[(i * (constants.GROUND.SUB + 1) + j) * 3 + 2] = this.height.get(j, -i) * 10
+      }
     }
 
     var plane = new Mesh(this.geometry, this.material)
 
     plane.rotation.x = Math.PI * 0.5
-
+    plane.rotation.z = Math.PI
     this.add(plane)
-    for (let i = 0; i < 1000; i++) {
-      const x = Math.random() * constants.GROUND.SIZE - constants.GROUND.SIZE / 2
-      const y = Math.random() * constants.GROUND.SIZE - constants.GROUND.SIZE / 2
-      this.add(new Vegetation({ size: 3, position: new Vector3(x, this.getHeight(x, y), y) }))
+
+    for (let i = 0; i < 100; i++) {
+      const x = (Math.random() - 0.5) * constants.GROUND.SIZE
+      const y = (Math.random() - 0.5) * constants.GROUND.SIZE
+      this.add(new Vegetation({ size: 3, position: new Vector3(x, this.getHeight(x, y) - 3, y) }))
     }
   }
 
   getHeight (x, y) {
-    const ratio = constants.GROUND.SIZE / constants.GROUND.SUB
-    return this.height.get(y * ratio - constants.GROUND.SIZE / 2, x * ratio - constants.GROUND.SUB) * 10
+    const ratio = (constants.GROUND.SUB) / constants.GROUND.SIZE
+    return this.height.get((x - constants.GROUND.SIZE * 0.5) * ratio, (-y - constants.GROUND.SIZE * 0.5) * ratio) * 10
   }
 
   update (time) {
+    // this.rotateY(0.04)
     // this.material.uniforms.uTime.value += 0.01
     // this.material.uniforms.uAmplitude.value = audio.volumeconst vertices = geometry.attributes.position.array
   }
