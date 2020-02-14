be4a9a9bf59c55ade7b4a940ee6a91fb8e954baa
Axel Donadio <axel@agence-revolutions.com>
2020-02-14 12:00:22 +0100

    prepare to merge

---
+++ src/webgl/ADNSelector.js
@@ -0,0 +1,37 @@
+import ADN from './objects/ADN'
+// import Cursor from 'utils/cursor'
+
+export default class ADNSelector {
+  constructor ($parent) {
+    this.parent = document.querySelector('#' + $parent)
+    this.defaultADN = new ADN()
+    this.cursors = []
+    this.init()
+  }
+
+  init () {
+    this.iterateOverADN(this.defaultADN.store)
+    this.drawSelectorCard()
+  }
+
+  iterateOverADN (obj) {
+    return Object.keys(obj).reduce((acc, key) => {
+      if (typeof obj[key] === 'object') {
+        this.iterateOverADN(obj[key])
+      } else if (Object.prototype.hasOwnProperty.call(obj, key) && !isNaN(parseFloat(obj[key]))) {
+        this.addCursor(key, obj[key])
+      }
+      return acc
+    }, [])
+  }
+
+  addCursor (name, value) {
+    // this.cursors.push(new Cursor(name, value))
+  }
+
+  drawSelectorCard () {
+    for (let i = 0; i < this.cursors.length; i++) {
+      this.parent.appendChild(this.cursors[i].DOM)
+    }
+  }
+}
